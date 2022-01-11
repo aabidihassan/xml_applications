5a624eb77d816b8e690d265d74ccf90c90ab994a
aabidihassan <aabidihassan2@gmail.com>
2022-01-11 16:29:19 +0100

    last commit

--- static/convertion/sql/result.sql
+++ static/convertion/sql/result.sql
@@ -1,4 +1,12 @@
-DROP TABLE IF EXISTS root;              
-                CREATE TABLE root(root_id INT NOT NULL, idAdmin VARCHAR2 (*),idClient VARCHAR2 (*),idGestionnaire VARCHAR2 (*),referenceCmd VARCHAR2 (*),dateCmd DATE,referenceProd VARCHAR2 (*),referenceCat VARCHAR2 (*),nomCat VARCHAR2 (*),referenceProd VARCHAR2 (*),libelle VARCHAR2 (*),dateProduit DATE,
-                PRIMARY KEY(root_id ) );
+DROP TABLE IF EXISTS Root;              
+                CREATE TABLE Root(Root_id INT NOT NULL, 
+                PRIMARY KEY(Root_id ) );
+                
+                DROP TABLE IF EXISTS Customers;              
+                CREATE TABLE Customers(Customers_id INT NOT NULL, CompanyName VARCHAR2 (*),ContactName VARCHAR2 (*),ContactTitle VARCHAR2 (*),Phone VARCHAR2 (*),Fax VARCHAR2 (*),Address VARCHAR2 (*),City VARCHAR2 (*),Region VARCHAR2 (*),PostalCode VARCHAR2 (*),Country VARCHAR2 (*),
+                PRIMARY KEY(Customers_id ) );
+                
+                DROP TABLE IF EXISTS Orders;              
+                CREATE TABLE Orders(Orders_id INT NOT NULL, CustomerID VARCHAR2 (*),EmployeeID VARCHAR2 (*),OrderDate DATETIME,RequiredDate DATETIME,ShipVia INTEGER (10,0),Freight DECIMAL(30,15),ShipName VARCHAR2 (*),ShipAddress VARCHAR2 (*),ShipCity VARCHAR2 (*),ShipRegion VARCHAR2 (*),ShipPostalCode VARCHAR2 (*),ShipCountry VARCHAR2 (*),
+                PRIMARY KEY(Orders_id ) );
                 
